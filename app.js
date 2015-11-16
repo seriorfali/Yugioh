3863726c49e2184dad63d1c16fad46368574016f
Seri <seriorfali@gmail.com>
2015-11-16 12:34:12 -0800

    Made views folder with layout.ejs file; added server-starting function, middleware, ejs view engine to app.js.

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var express = require('express')
-var app     = express()
+var
+  express = require('express'),
+  app     = express()
+
+// App configurations.
+app.set("view engine", "ejs");
+
+// Middleware.
+app.use(ejsLayouts);
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(function(req, res, next) {
+  console.log("A request has been made.");
+});
+
+// To start server on port 3000 of localhost.
+app.listen(3000, function() {
+  console.log("Server running on 3000.");
+});
