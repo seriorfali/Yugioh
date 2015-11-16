db18b492623122080dbae926d78dc26e8ad74b43
Seri <seriorfali@gmail.com>
2015-11-15 21:55:46 -0800

    Added show, add, update, and destroy actions to cafes controller.

--- controllers/cafesController.js
+++ controllers/cafesController.js
@@ -2,11 +2,43 @@ var Cafe = require('../models/cafe.js');
 
 function index(req, res) {
   Cafe.find({}, function(err, cafes) {
-    if (err) throw err;
+    if (err) res.send(err);
     res.json(cafes);
   });
 }
 
+function show(req, res) {
+  Cafe.findById{req.params.id}, function(err, cafe) {
+    if (err) res.send(err);
+    res.json(cafe);
+  });
+}
+
+function add(req, res) {
+  Cafe.create(req.body, function(err, cafe) {
+    if (err) res.send(err);
+    res.json({success: true, message: cafe.name + "has been added."});
+  });
+}
+
+function update(req, res) {
+  Cafe.findOneAndUpdate({id: req.params.id}, req.body, {new: true}, function(err, cafe) {
+    if (err) res.send(err);
+    res.json({success: true, message: cafe.name + "has been updated."});
+  });
+}
+
+function destroy(req, res) {
+  Cafe.findOneAndRemove({id: req.params.id}, function(err, cafe) {
+    if (err) console.log(err);
+    res.json({success: true, message: cafe.name + "has been deleted."});
+  });
+}
+
 module.exports = {
-  index: index
+  index: index,
+  show: show,
+  add: add,
+  update: update,
+  destroy: destroy
 };
